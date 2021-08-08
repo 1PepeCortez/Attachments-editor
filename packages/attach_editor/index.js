8c500a0940da5d7f04d456cd4af19f8fed99d517
1PepeCortez <67793531+1PepeCortez@users.noreply.github.com>
2021-08-08 19:01:05 +0200

    Changed save message

--- packages/attach_editor/index.js
+++ packages/attach_editor/index.js
@@ -70,7 +70,7 @@ mp.events.addCommand('attach', (player, _, object, body) => {
 mp.events.add('finishAttach', (player, object) => {
 
 	let objectJSON = JSON.parse(object);
-	let text = `attachTo('${objectJSON.object}', ${objectJSON.body}, ${objectJSON.x.toFixed(4)}, ${objectJSON.y.toFixed(4)}, ${objectJSON.z.toFixed(4)}, ${objectJSON.rx.toFixed(4)}, ${objectJSON.ry.toFixed(4)}, ${objectJSON.rz.toFixed(4)}, ...);\r\n`;
+	let text = `{ ${objectJSON.object}, ${objectJSON.body}, ${objectJSON.x.toFixed(4)}, ${objectJSON.y.toFixed(4)}, ${objectJSON.z.toFixed(4)}, ${objectJSON.rx.toFixed(4)}, ${objectJSON.ry.toFixed(4)}, ${objectJSON.rz.toFixed(4)} },\r\n`;
 	
 	player.outputChatBox(text);
 
